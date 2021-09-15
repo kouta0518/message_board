ad5e839f94a8549651e8b893586a9f5261f7e821
kouta <>
2021-09-15 14:28:09 +0900

    フラッシュメッセージを追加

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -18,15 +18,15 @@ import utils.DBUtil;
  */
 @WebServlet("/update")
 public class UpdateServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     public UpdateServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    String _token = request.getParameter("_token");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = request.getParameter("_token");
         if(_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
 
@@ -47,6 +47,7 @@ public class UpdateServlet extends HttpServlet {
             // データベースを更新
             em.getTransaction().begin();
             em.getTransaction().commit();
+            request.getSession().setAttribute("flush", "更新が完了しました。");
             em.close();
 
             // セッションスコープ上の不要になったデータを削除
@@ -55,6 +56,6 @@ public class UpdateServlet extends HttpServlet {
             // indexページへリダイレクト
             response.sendRedirect(request.getContextPath() + "/index");
         }
-	}
+    }
 
 }
